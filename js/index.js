7e18d77b0d21cfa3d5bf288f2a6bead5bc5a9f2b
geoffrey woodcock <theatomproject@gmail.com>
2019-06-04 13:20:58 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ function retrieveGithubUsername(){
   return document.getElementById('username').value
 }
 
+function getRepositories() {
+  const usernameInput = retrieveGithubUsername()
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${usernameInput}/repos`);
+  req.send();
+}
